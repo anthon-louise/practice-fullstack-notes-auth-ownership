1fddd3f00cf9d1add0e677b1ae97d660485ad4ba
anthon-dev <anthonlouisenoynay123@example.com>
2026-08-16 18:51:17 +0800

    login user axios

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,7 +1,12 @@
-import type { registerInput } from "../schemas/auth";
+import type { loginInput, registerInput } from "../schemas/auth";
 import { api } from "./axios";
 
 export const registerUser = async (data: registerInput) => {
   const res = await api.post("/auth/register", data);
+  return res.data;
+}
+
+export const loginUser = async (data: loginInput) => {
+  const res = await api.post("/auth/login", data)
   return res.data;
 }
