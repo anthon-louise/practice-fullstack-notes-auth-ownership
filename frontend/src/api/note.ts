31341038cca8aa287eae6cb6d1f9955ff36323f7
anthon-dev <anthonlouisenoynay123@example.com>
2026-09-04 08:00:55 +0800

    get note by id axios

--- frontend/src/api/note.ts
+++ frontend/src/api/note.ts
@@ -5,3 +5,9 @@ export const getNotes = async (): Promise<Note[]> => {
   const res = await api.get("/note");
   return res.data.notes;
 }
+
+export const getNoteById = async (id: number): Promise<Note> => {
+  const res = await api.get(`/note/${id}`);
+  return res.data.notes;
+}
+
